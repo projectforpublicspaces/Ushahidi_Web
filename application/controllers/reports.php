c54766c91a9009e96a3df4edff104378347347d3
Robert Marianski <rob@marianski.com>
2012-05-31 10:09:08 -0500

    correct issue where proper categories weren't showing up on search

--- application/controllers/reports.php
+++ application/controllers/reports.php
@@ -77,9 +77,15 @@ class Reports_Controller extends Main_Controller {
 		} else {
                   $default_cat_search_query = ORM::factory('category')->where('category_title', 'Lighter Quick Cheaper')->find_all();
                   foreach ($default_cat_search_query as $cat) {
-                    $allowed_ids[] = $cat->id;
+                    $query = 'SELECT ic.incident_id AS incident_id FROM '.$this->table_prefix.'incident_category AS ic INNER JOIN '.$this->table_prefix.'category AS c ON (ic.category_id = c.id)  WHERE c.id='.$cat->id.';';
+                    $query = $db->query($query);
+                    foreach ( $query as $items )
+                    {
+                      $allowed_ids[] = $items->incident_id;
+                    }
+                    //$allowed_ids[] = $cat->id;
                   }
-        }
+                }
 
 		// Get location_ids if we are to filter by location
 		$location_ids = array();
